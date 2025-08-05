9ebc24deca2cb2b8962e2d3345426f339a68f8d6
Carrasco-Jorge <carrascoislasjorge@gmail.com>
2025-08-05 10:56:17 -0600

    Organize database models and engines

---
+++ src/qqp/database/db_models.py
@@ -0,0 +1,94 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, Float, Date
+from qqp.database.engines import db_engine
+
+class DBTables:
+    engine = db_engine
+    metadata = MetaData()
+
+    units = Table(
+        "units",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(45), nullable=False),
+        Column("unit", String(45), nullable=False),
+        Column("measures", String(45), nullable=False),
+        Column("std_unit", String(45), nullable=False),
+        Column("std_quantity", Integer, nullable=False)
+    )
+
+    processed_files = Table(
+        "processed_files",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(45), nullable=False)
+    )
+
+    product_presentations = Table(
+        "product_preentations",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("product", String(100), nullable=False),
+        Column("presentation", String(120), nullable=False),
+        Column("processed", Integer, nullable=False),
+        Column("variant", String(45)),
+        Column("unit", String(45)),
+        Column("quantity", Integer)
+    )
+
+    pending_rows = Table(
+        "pending_rows",
+        metadata,
+        Column("product", String(100), nullable=False),
+        Column("presentation", String(120), nullable=False),
+        Column("brand", String(50), nullable=False),
+        Column("type", String(45), nullable=False),
+        Column("category", String(45), nullable=False),
+        Column("price", Float, nullable=False),
+        Column("date", Date, nullable=False),
+        Column("chain", String(45), nullable=False),
+        Column("line_of_business", String(50), nullable=False),
+        Column("branch", String(100), nullable=False),
+        Column("address", String(120), nullable=False),
+        Column("state", String(45), nullable=False),
+        Column("municipality", String(45), nullable=False),
+        Column("lat", Float, nullable=False),
+        Column("long", Float, nullable=False)
+    )
+
+    dim_products = Table(
+        "dim_products",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("category", String(45), nullable=False),
+        Column("type", String(45), nullable=False),
+        Column("product", String(100), nullable=False),
+        Column("variant", String(45), nullable=False),
+        Column("brand", String(50), nullable=False),
+        Column("std_unit", String(45), nullable=False),
+        Column("std_quantity", Float, nullable=False)
+    )
+
+    dim_stores = Table(
+        "dim_stores",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("line_of_business", String(50), nullable=False),
+        Column("chain", String(45), nullable=False),
+        Column("branch", String(100), nullable=False),
+        Column("address", String(120), nullable=False),
+        Column("lat", Float, nullable=False),
+        Column("long", Float, nullable=False)
+    )
+
+    fact_price = Table(
+        "fact_price",
+        metadata,
+        Column("date", Date, nullable=False),
+        Column("product_id", Integer, primary_key=True),
+        Column("store_id", Integer, primary_key=True),
+        Column("std_price", Float, nullable=False)
+    )
+
+    @classmethod
+    def create_tables(cls):
+        cls.metadata.create_all(cls.engine)
---
+++ src/qqp/database/sqlite_models.py
@@ -0,0 +1,76 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, Float, Date
+from qqp.database.engines import sqlite_engine
+
+class SQLiteTables:
+    engine = sqlite_engine
+    metadata = MetaData()
+
+    product_presentations = Table(
+        "product_preentations",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("product", String(100), nullable=False),
+        Column("presentation", String(120), nullable=False),
+        Column("processed", Integer, nullable=False),
+        Column("variant", String(45)),
+        Column("unit", String(45)),
+        Column("quantity", Integer)
+    )
+
+    pending_rows = Table(
+        "pending_rows",
+        metadata,
+        Column("product", String(100), nullable=False),
+        Column("presentation", String(120), nullable=False),
+        Column("brand", String(50), nullable=False),
+        Column("type", String(45), nullable=False),
+        Column("category", String(45), nullable=False),
+        Column("price", Float, nullable=False),
+        Column("date", Date, nullable=False),
+        Column("chain", String(45), nullable=False),
+        Column("line_of_business", String(50), nullable=False),
+        Column("branch", String(100), nullable=False),
+        Column("address", String(120), nullable=False),
+        Column("state", String(45), nullable=False),
+        Column("municipality", String(45), nullable=False),
+        Column("lat", Float, nullable=False),
+        Column("long", Float, nullable=False)
+    )
+
+    dim_products = Table(
+        "dim_products",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("category", String(45), nullable=False),
+        Column("type", String(45), nullable=False),
+        Column("product", String(100), nullable=False),
+        Column("variant", String(45), nullable=False),
+        Column("brand", String(50), nullable=False),
+        Column("std_unit", String(45), nullable=False),
+        Column("std_quantity", Float, nullable=False)
+    )
+
+    dim_stores = Table(
+        "dim_stores",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("line_of_business", String(50), nullable=False),
+        Column("chain", String(45), nullable=False),
+        Column("branch", String(100), nullable=False),
+        Column("address", String(120), nullable=False),
+        Column("lat", Float, nullable=False),
+        Column("long", Float, nullable=False)
+    )
+
+    fact_price = Table(
+        "fact_price",
+        metadata,
+        Column("date", Date, nullable=False),
+        Column("product_id", Integer, primary_key=True),
+        Column("store_id", Integer, primary_key=True),
+        Column("std_price", Float, nullable=False)
+    )
+
+    @classmethod
+    def create_tables(cls):
+        cls.metadata.create_all(cls.engine)
